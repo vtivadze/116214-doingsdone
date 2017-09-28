0626ab29d43d1e7e7c8c11c2dcbbfd5ac2766586
Vakhtang Tivadze <vaxotivadze@gmail.com>
2017-09-29 02:02:35 +0400

    Decreased 'if' block counts

--- index.php
+++ index.php
@@ -102,217 +102,212 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && $con) {
 
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && $con) {
-    if (isset($_POST['add']) && $_POST['add'] == 'Добавить') {
 
-        $name = trim($_POST['name']);
-        $project = trim($_POST['project']);
-        $date = trim($_POST['date']);
+if ($con && isset($_POST['add']) && $_POST['add'] == 'Добавить') {
 
-        $required = ['name', 'project'];
-        $rules = 
+    $name = trim($_POST['name']);
+    $project = trim($_POST['project']);
+    $date = trim($_POST['date']);
+
+    $required = ['name', 'project'];
+    $rules = 
+    [
+        'name' => 'validateName',
+        'project' => 'validateProject',
+        'date' => 'validateDate',
+    ];
+
+    $errors = [];
+    foreach ($_POST as $key => $value) {
+        if (in_array($key, $required) && $value == '') {
+            formErrors($errors, $key, 'Заполните это поле!');
+            continue;
+        }
+
+        if (array_key_exists($key, $rules) && !call_user_func($rules[$key], $value)) {
+            formErrors($errors, $key, 'Заполните поле корректно!');
+        }
+    }
+
+    if (isset($_FILES['preview']) && $_FILES['preview']['error'] != 4) {
+        if (call_user_func('validateFile', $_FILES['preview'])) {
+            $f_name = $_FILES['preview']['name'];
+            $f_tmp_name = $_FILES['preview']['tmp_name'];
+            move_uploaded_file($f_tmp_name, $f_name);
+        }
+        else {
+            formErrors($errors, 'preview', 'Некорректный фаил!');
+        }
+    }
+
+    if (!count($errors)) {
+        $res = insertData($con, 'tasks', [
+            'name' => $name,
+            'file' => $f_name ?? '',
+            'deadline' => $date ? formDate($date) : '',
+            'proj_id' => (int)$project
+        ]);
+
+        header("Location: /");
+        exit;
+
+    } else {
+
+        $overlay = 'overlay';
+        $content = render('add_task', 
         [
-            'name' => 'validateName',
-            'project' => 'validateProject',
-            'date' => 'validateDate',
-        ];
+            'projects' => $projects,
+            'project' => $project ?? '',
+            'name' => $name ?? '',
+            'date' => $date ?? '',
+            'errors' => $errors ?? []
+        ]);
 
-        $errors = [];
-        foreach ($_POST as $key => $value) {
-            if (in_array($key, $required) && $value == '') {
-                formErrors($errors, $key, 'Заполните это поле!');
-                continue;
-            }
-
-            if (array_key_exists($key, $rules)) {
-                if (!call_user_func($rules[$key], $value)) {
-                    formErrors($errors, $key, 'Заполните поле корректно!');
-                }
-            }
-        }
-
-        if (isset($_FILES['preview']) && $_FILES['preview']['error'] != 4) {
-            if (call_user_func('validateFile', $_FILES['preview'])) {
-                $f_name = $_FILES['preview']['name'];
-                $f_tmp_name = $_FILES['preview']['tmp_name'];
-                move_uploaded_file($f_tmp_name, $f_name);
-            }
-            else
-                formErrors($errors, 'preview', 'Некорректный фаил!');
-        }
-
-        if (!count($errors)) {
-            $res = insertData($con, 'tasks', [
-                'name' => $name,
-                'file' => $f_name ?? '',
-                'deadline' => $date ? formDate($date) : '',
-                'proj_id' => (int)$project
-            ]);
-
-            header("Location: /");
-            exit;
-
-        } else {
-
-            $overlay = 'overlay';
-            $content = render('add_task', 
-            [
-                'projects' => $projects,
-                'project' => $project ?? '',
-                'name' => $name ?? '',
-                'date' => $date ?? '',
-                'errors' => $errors ?? []
-            ]);
-
-        }
-    }
-
-    if (isset($_POST['login'])) {
-
-        $email = trim($_POST['email']);
-        $password = trim($_POST['password']);
-
-        $required = ['email', 'password'];
-        $rules = ['email' => 'validateEmail'];
-
-        $errors = [];
-        foreach ($_POST as $key => $value) {
-            if (in_array($key, $required) && $value == '') {
-                formErrors($errors, $key, 'Заполните это поле!');
-                continue;
-            }
-
-            if (array_key_exists($key, $rules)) {
-                if (!call_user_func($rules[$key], $value)) {
-                    formErrors($errors, $key, 'Заполните поле корректно!');
-                }
-            }
-        }
-
-        if (!count($errors)) {
-            
-            $user_dt = selectData($con, 'SELECT id, name, password FROM users WHERE email = ?', [$email]);
-            
-            if ($user_dt && password_verify($password, $user_dt[0]['password'])) {
-                $_SESSION['email'] = $email;
-                $_SESSION['name'] = $user_dt[0]['name'];
-                $_SESSION['user_id'] = $user_dt[0]['id'];
-
-                header("Location: /");
-                exit;
-            } else {
-                $overlay = 'overlay';
-                $hidden = '';
-                $errors['password']['msg'] = "Вы ввели неверный пароль!";
-                $errors['password']['class'] = "form__input--error";
-
-            }
-
-        } else {
-
-            $overlay = 'overlay';
-            $hidden = '';
-
-        }
-    }
-
-    if (isset($_POST['register'])) {
-        
-        $email = trim($_POST['email']);
-        $password = trim($_POST['password']);
-        $name = trim($_POST['name']);
-
-        $required = ['email', 'password', 'name'];
-        $rules = ['email' => 'validateEmail'];
-
-        $errors = [];
-        foreach ($_POST as $key => $value) {
-            if (in_array($key, $required) && $value == '') {
-                formErrors($errors, $key, 'Заполните это поле!');
-                continue;
-            }
-
-            if (array_key_exists($key, $rules)) {
-                if (!call_user_func($rules[$key], $value)) {
-                    formErrors($errors, $key, 'Заполните поле корректно!');
-                }
-            }
-        }
-
-
-        if (selectData($con, 'SELECT id FROM users WHERE email = ?', [$email])) {
-            $errors['email'] = [
-                'class' => 'form__input--error',
-                'msg' => 'Email используется другим пользователем!'
-            ];
-        }
-
-        if (!count($errors)) {
-            
-            insertData($con, 'users', [
-                'email' => $email,
-                'name' => $name,
-                'password' => password_hash($password, PASSWORD_DEFAULT)
-            ]); 
-
-            $registered = selectData($con, 'SELECT name, password FROM users WHERE email = ?', [$email]);
-
-        }
-    }
-
-    if (isset($_POST['add_project']) && $_POST['add_project'] == 'Добавить') {
-        if (!isset($_SESSION['name'])) {
-            header("Location: logout.php");
-            exit;
-        }
-
-        $user_id = (int)$_SESSION['user_id'];
-        $proj_name = trim($_POST['name']);
-        $required = ['name'];
-
-        $errors = [];
-        foreach ($_POST as $key => $value) {
-            if (in_array($key, $required) && $value == '') {
-                formErrors($errors, $key, 'Заполните это поле!');
-                continue;
-            }
-        }
-
-
-        if (!count($errors)) {
-            $res = insertData($con, 'projects', [
-                'name' => $proj_name,
-                'user_id' => $user_id
-            ]);
-
-            header("Location: /");
-            exit;
-
-        } else {
-
-            $overlay = 'overlay';
-            $content = render('add_project', 
-            [
-                'name' => $name ?? '',
-                'errors' => $errors ?? []
-            ]);
-        }
-    }
-
-    if (isset($_POST['search_task']) && $_POST['search_task'] == 'Искать') {
-        $task_name = trim($_POST['task_name']);
-        if ($task_name != '') {
-            $task_name = '%'.$task_name.'%';
-            $found_tasks = selectData($con, "SELECT * FROM tasks WHERE name LIKE ?", [$task_name]);
-
-            $content = render('index', 
-            [
-                'show_complete_tasks' => $show_complete_tasks,
-                'tasks' => $found_tasks
-            ]);
-        }
     }
 }
+
+if ($con && isset($_POST['login'])) {
+
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    $required = ['email', 'password'];
+    $rules = ['email' => 'validateEmail'];
+
+    $errors = [];
+    foreach ($_POST as $key => $value) {
+        if (in_array($key, $required) && $value == '') {
+            formErrors($errors, $key, 'Заполните это поле!');
+            continue;
+        }
+
+        if (array_key_exists($key, $rules) && !call_user_func($rules[$key], $value)) {
+            formErrors($errors, $key, 'Заполните поле корректно!');
+        }
+    }
+
+    if (!count($errors)) {
+        
+        $user_dt = selectData($con, 'SELECT id, name, password FROM users WHERE email = ?', [$email]);
+        
+        if ($user_dt && password_verify($password, $user_dt[0]['password'])) {
+            $_SESSION['email'] = $email;
+            $_SESSION['name'] = $user_dt[0]['name'];
+            $_SESSION['user_id'] = $user_dt[0]['id'];
+
+            header("Location: /");
+            exit;
+        } else {
+            $overlay = 'overlay';
+            $hidden = '';
+            $errors['password']['msg'] = "Вы ввели неверный пароль!";
+            $errors['password']['class'] = "form__input--error";
+
+        }
+
+    } else {
+
+        $overlay = 'overlay';
+        $hidden = '';
+
+    }
+}
+
+if ($con && isset($_POST['register'])) {
+    
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $name = trim($_POST['name']);
+
+    $required = ['email', 'password', 'name'];
+    $rules = ['email' => 'validateEmail'];
+
+    $errors = [];
+    foreach ($_POST as $key => $value) {
+        if (in_array($key, $required) && $value == '') {
+            formErrors($errors, $key, 'Заполните это поле!');
+            continue;
+        }
+
+        if (array_key_exists($key, $rules) && !call_user_func($rules[$key], $value)) {
+            formErrors($errors, $key, 'Заполните поле корректно!');
+        }
+    }
+
+
+    if (selectData($con, 'SELECT id FROM users WHERE email = ?', [$email])) {
+        $errors['email'] = [
+            'class' => 'form__input--error',
+            'msg' => 'Email используется другим пользователем!'
+        ];
+    }
+
+    if (!count($errors)) {
+        
+        insertData($con, 'users', [
+            'email' => $email,
+            'name' => $name,
+            'password' => password_hash($password, PASSWORD_DEFAULT)
+        ]); 
+
+        $registered = selectData($con, 'SELECT name, password FROM users WHERE email = ?', [$email]);
+
+    }
+}
+
+if ($con && isset($_POST['add_project']) && $_POST['add_project'] == 'Добавить') {
+    if (!isset($_SESSION['name'])) {
+        header("Location: logout.php");
+        exit;
+    }
+
+    $user_id = (int)$_SESSION['user_id'];
+    $proj_name = trim($_POST['name']);
+    $required = ['name'];
+
+    $errors = [];
+    foreach ($_POST as $key => $value) {
+        if (in_array($key, $required) && $value == '') {
+            formErrors($errors, $key, 'Заполните это поле!');
+            continue;
+        }
+    }
+
+
+    if (!count($errors)) {
+        $res = insertData($con, 'projects', [
+            'name' => $proj_name,
+            'user_id' => $user_id
+        ]);
+
+        header("Location: /");
+        exit;
+
+    } else {
+
+        $overlay = 'overlay';
+        $content = render('add_project', 
+        [
+            'name' => $name ?? '',
+            'errors' => $errors ?? []
+        ]);
+    }
+}
+
+if ($con && isset($_POST['search_task']) && $_POST['search_task'] == 'Искать') {
+    $task_name = trim($_POST['task_name']);
+    if ($task_name != '') {
+        $task_name = '%'.$task_name.'%';
+        $found_tasks = selectData($con, "SELECT * FROM tasks WHERE name LIKE ?", [$task_name]);
+
+        $content = render('index', 
+        [
+            'show_complete_tasks' => $show_complete_tasks,
+            'tasks' => $found_tasks
+        ]);
+    }
+}
+
 
 $header = render('header', []);
 
@@ -351,7 +346,6 @@ if (!isset($_SESSION['name'])) {
             'errors' => $errors ?? []
         ]);
     }
-
 
 } else {
 
